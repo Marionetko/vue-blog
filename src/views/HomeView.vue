7d7b879de5f1e947895c3789bbb557e01513806b
Marionetko <webius.net@gmail.com>
2023-05-25 16:55:12 +0300

    upd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,42 +1,22 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search form - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">
-      {{ name }}
-    </div>
-    <button @click="handleClick">stop watching</button>
+
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue';
+import { ref } from 'vue';
 
 export default {
   name: 'Home',
   setup() {
-    const search = ref('');
-    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach']);
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1},
+      { title: 'top 5 css tips', body: 'Lorem ipsum', id: 2},
+    ])
 
-    const stopWatch = watch(search, () => {
-      console.log('watcher');
-    })
-
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect', search.value)
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value));
-    })
-
-    const handleClick = () => {
-      stopWatch();
-      stopEffect();
-    }
-
-    return { names, search, matchingNames, handleClick }
+    return { posts }
   }
 }
 </script>
