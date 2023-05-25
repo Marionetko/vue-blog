51be278865ec4d83e4524505860479dfc8924249
Marionetko <webius.net@gmail.com>
2023-05-25 16:18:42 +0300

    reactive vs refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,27 +4,34 @@
     <h2>Refs</h2>
     <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
     <button @click="updateNinjaOne">Click!</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 export default {
   name: 'Home',
   setup() {
     const ninjaOne = ref({ name: 'mario', age: 30 });
+    const ninjaTwo = reactive({ name: 'luigi', age: 35});
+
+    const nameOne = ref('mario');
+    const nameTwo = reactive('luigi'); //not reactive (can't use primitive value)
 
     const updateNinjaOne = () => {
       ninjaOne.value.age = 40;
     }
 
-    return { ninjaOne, updateNinjaOne }
-  },
-  data() {
-    return {
-      score: 5
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 45;
+      nameTwo = 'mario'; // not working
     }
+
+    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo, nameTwo }
   }
 }
 </script>
