e839860e96f9f5aa44d2664e897db8155427cb54
Marionetko <webius.net@gmail.com>
2023-05-25 16:30:12 +0300

    computed values

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-
+    <input type="text" v-model="search">
+    <p>search form - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 export default {
   name: 'Home',
   setup() {
+    const search = ref('');
+    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach']);
 
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    })
+
+    return { names, search, matchingNames }
   }
 }
 </script>
